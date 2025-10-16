77e65b89a94e4c638d938b1ff840aac4c1133e07
Nayen Roman <nay.roman@duocuc.cl>
2025-10-16 09:13:21 -0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import './styles/global.css'; 
+import './styles/global.css'; // 💡 Import global aquí
+import './styles/App.css';    // luego los específicos
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
 );
+
