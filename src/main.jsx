771fa6c0f7cbc7cf6f5bc64fcba42cf0a38f082d
Nayen Roman <nay.roman@duocuc.cl>
2025-10-16 09:06:22 -0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,10 @@
 import React from 'react';
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import { BrowserRouter } from 'react-router-dom';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './styles/global.css';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import './styles/global.css'; 
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
